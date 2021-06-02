3b4977f7f60f8863a5a28eb1d96c338ff752dd09
Bleriot Noguia <nstevebleriot@yahoo.fr>
2021-06-02 13:22:29 +0100

    react-tooltip added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import { FaChartPie, FaMoon, FaList } from "react-icons/fa"
+import { FaChartPie, FaMoon, FaList, FaSyncAlt } from "react-icons/fa"
+import ReactTooltip from 'react-tooltip'
 import MyListBox from "./MyListBox.jsx"
 
 export default function Navbar() {
@@ -12,16 +13,23 @@ export default function Navbar() {
             </div>
             <ul className="flex items-center">
                 <li className="mr-2.5">
-                    <Link to="/marketcap"><FaList /></Link>
+                    <Link to="/marketcap" data-tip="Marketcap" data-for="marketcap"><FaList /></Link>
+                    <ReactTooltip id="marketcap" place="bottom" type="dark" effect="solid" />
                 </li>
                 <li className="p-2">
-                    <Link to="/portfolio"><FaChartPie /></Link>
+                    <Link to="/portfolio" data-tip="Protfolio" data-for="portfolio"><FaChartPie /></Link>
+                    <ReactTooltip id="portfolio" place="bottom" type="dark" effect="solid" />
                 </li>
                 <li>
                     <MyListBox />
                 </li>
                 <li className="p-2">
-                    <FaMoon />
+                    <FaMoon data-tip="Switch theme" data-for="theme" />
+                    <ReactTooltip id="theme" place="bottom" type="dark" effect="solid" />
+                </li>
+                <li className="p-2">
+                    <FaSyncAlt data-tip="Refresh data" data-for="refresh" />
+                    <ReactTooltip id="refresh" place="bottom" type="dark" effect="solid" />
                 </li>
             </ul>
         </div>
